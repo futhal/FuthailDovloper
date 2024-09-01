04b99c38c25f65abbd84e3badba81597eda983a3
futhal <75042617+futhal@users.noreply.github.com>
2024-09-01 19:31:24 +0000

    ff

--- demo/settings.py
+++ demo/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if ENVIRONMENT == 'production':
     DEBUG = True
